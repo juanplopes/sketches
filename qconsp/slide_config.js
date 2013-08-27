ea3fbf690e28a35c2aa07373ad8fb075a5d717d3
Juan Lopes <me@juanlopes.net>
2013-08-26 23:55:24 -0300

    first 7 slides

--- qconsp/slide_config.js
+++ qconsp/slide_config.js
@@ -1,18 +1,17 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'Analisando e reduzindo grandes fluxos de dados em tempo real',
+    subtitle: 'Como um pouco de álgebra pode ajudar',
+    eventInfo: {
+      title: 'QCon SP',
+      date: '8/30/2013'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'intelie_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,19 +21,11 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }*/]
+    name: 'Juan Lopes',
+    company: 'Intelie',
+    twitter: '@juanplopes',
+    www: 'http://juanlopes.net',
+    github: 'http://github.com/juanplopes'
+  }]
 };
 
