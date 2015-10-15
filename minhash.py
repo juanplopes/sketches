bb16e8eee66903e6c918bc1874012c77213ea111
Juan Lopes <me@juanlopes.net>
2015-10-14 22:19:50 -0300

    single hash function version

--- minhash.py
+++ minhash.py
@@ -1,4 +1,4 @@
-import shakespeare, sys, mmh3, math, collections
+import shakespeare, sys, mmh3, math, collections, heapq
 
 def normal_compare(A, B):
     SA = set(A)
@@ -13,10 +13,21 @@ def minhash1(k, A, B):
     HB = minhash1_sig(k, B)
     return sum(a==b for a, b in zip(HA, HB))/float(k)
 
+def minhash2_sig(k, A):
+    return heapq.nsmallest(k, (mmh3.hash(str(word), 0) for word in A))
+
+def minhash2(k, A, B):
+    HA = minhash2_sig(k, A)
+    HB = minhash2_sig(k, B)
+    HX = heapq.nsmallest(k, set(HA).union(HB))
+    HY = set(HX).intersection(HA).intersection(HB)
+    return len(HY)/float(k)
+
+
 if __name__ == '__main__':
     works = list(shakespeare.each_work())
     
-    for k in range(25, 1001, 25):
+    for k in range(25, 2001, 25):
         sys.stderr.write(str(k)+'\n')
         T = [k]
         for i in range(len(works)):
@@ -24,7 +35,7 @@ if __name__ == '__main__':
                 name1, words1 = works[i]
                 name2, words2 = works[j]
                 normal = normal_compare(words1, words2)
-                mh1 = minhash1(k, words1, words2)
+                mh1 = minhash2(k, words1, words2)
                 T.append(mh1-normal)
         print '\t'.join(map(str, T))
 
