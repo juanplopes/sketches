282a59a1b78055b69b9aecd7409c6cad1f69ea87
Juan Lopes <me@juanlopes.net>
2017-01-25 14:36:02 -0200

    mhtree

--- graph_mh_tree.py
+++ graph_mh_tree.py
@@ -1,7 +1,13 @@
-import random, mmh3, collections, math, heapq
+import random, mmh3, collections, math, heapq, sys
+
+def bitsoncount(i):
+    assert 0 <= i < 0x100000000
+    i = i - ((i >> 1) & 0x55555555)
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333)
+    return (((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) & 0xffffffff) >> 24
 
 def gen_set(S, to):
-    return [S[2*i+bin(i&to).count('1')%2] for i in xrange(len(S)/2)]
+    return [S[2*i+bitsoncount(i&to)%2] for i in xrange(len(S)/2)]
 
 def unique_numbers():
     i = [0]
@@ -12,7 +18,7 @@ def unique_numbers():
     return gen
 
 def minhash_sig(A, k):
-    return [min(mmh3.hash(str(word), i) for word in A) for i in range(k)]
+    return [min(mmh3.hash(str(word), i) for word in A) for i in xrange(k)]
 
 def minhash(HA, HB, k):
     return sum(a==b for a, b in zip(HA, HB))/float(k)
@@ -27,9 +33,9 @@ def simhash_sig(A, k):
 def simhash(HA, HB, k):
     return (k-bin(HA^HB).count('1'))/float(k)
 
-def random_tree(n, k, sig):
+def random_tree(n, k):
     N = unique_numbers()
-    m = int(2**(math.ceil(math.log(n, 2))+1))
+    m = max(256, int(2**(math.ceil(math.log(n, 2))+1)))
     G = [(None, N(m))]
 
     for i in range(n-1):
@@ -37,27 +43,74 @@ def random_tree(n, k, sig):
         _, S = G[j]
     
         G.append((j, gen_set(S, i) if len(S) == m else S+N(m/2)))
-    return [(p, sig(S, k)) for p, S in G]
+    return [(p, minhash_sig(S, k), simhash_sig(S, k), len(S) == m) for p, S in G]
 
-def compute_error(N, k, sig, comp):
-    G = random_tree(N, k, sig)
+def compute_error(G, k, delta1, delta2):
+    N = len(G)
+    FN1, FN2, TN = 0, 0, 0
+    FP1, FP2, TP = 0, 0, 0
 
-    print G
-
-    total = 0
-    E = 0
-    EE = 0
     for i in range(N):
         for j in range(i+1, N):
             nei = G[i][0] == j or G[j][0] == i
-            value = comp(G[i][1], G[j][1], k)
-            ok = nei == (value >= 0.363198231)
-            if not ok: E += 1
-            if nei and not ok: EE += 1
-            print nei, value
-            total += 1
-    print E, EE, total
-    return float(E)/total
+            value1 = minhash(G[i][1], G[j][1], k)
+            value2 = simhash(G[i][2], G[j][2], k)
+            if not nei:
+                if value1 >= delta1: FP1+=1
+                if value2 >= delta2: FP2+=1
+            else:
+                if value1 <= delta1: FN1+=1
+                if value2 <= delta2: FN2+=1
+
+            if nei: TP += 1 
+            else: TN += 1
+
+    return (float(FP1)/TN, float(FN1)/TP, float(FP2)/TN, float(FN2)/TP)
     
-#print compute_error(200, 64, minhash_sig, minhash)
-print compute_error(10, 64, simhash_sig, simhash)
+def avg(L):
+    return float(sum(L))/len(L)
+    
+def test_delta(N, k):
+    X = 40
+    RP1 = [[] for i in xrange(X+1)]
+    RN1 = [[] for i in xrange(X+1)]
+    RP2 = [[] for i in xrange(X+1)]
+    RN2 = [[] for i in xrange(X+1)]
+
+    for test in range(100):
+        sys.stderr.write(str(test)+'\n')
+        G = random_tree(N, k)
+        for delta in range(X+1):
+            FP1, FN1, FP2, FN2 = compute_error(G, k, delta/float(X), delta/float(X))
+            RP1[delta].append(FP1)
+            RN1[delta].append(FN1)
+            RP2[delta].append(FP2)
+            RN2[delta].append(FN2)
+            
+    for delta in range(0, X+1):
+        print '\t'.join(str(x) for x in (delta/float(X), avg(RP1[delta]), avg(RN1[delta]), avg(RP2[delta]), avg(RN2[delta])))
+
+def test_size(k):
+    X = 40
+    RP1 = [[] for i in xrange(X+1)]
+    RN1 = [[] for i in xrange(X+1)]
+    RP2 = [[] for i in xrange(X+1)]
+    RN2 = [[] for i in xrange(X+1)]
+
+    for test in range(100):
+        for delta in range(1, X+1):
+            sys.stderr.write(str(test)+' '+str(delta)+'\n')    
+            N = int(delta/float(X)*200)
+            G = random_tree(N, k)
+            FP1, FN1, FP2, FN2 = compute_error(G, k, 0.416666667, 0.671173911)
+            RP1[delta].append(FP1)
+            RN1[delta].append(FN1)
+            RP2[delta].append(FP2)
+            RN2[delta].append(FN2)
+            
+    for delta in range(1, X+1):
+        N = int(delta/float(X)*200)
+        print '\t'.join(str(x) for x in (N, avg(RP1[delta]), avg(RN1[delta]), avg(RP2[delta]), avg(RN2[delta])))
+
+#test_delta(25, 128)
+test_size(128)
