41e83098304dbd2b03e95b01d844dc3d0c7fe5a1
Juan Lopes <me@juanlopes.net>
2018-02-25 23:26:28 -0300

    wa

--- graph_mh_tree.py
+++ graph_mh_tree.py
@@ -33,7 +33,7 @@ def simhash_sig(A, k):
 def simhash(HA, HB, k):
     return (k-bin(HA^HB).count('1'))/float(k)
 
-def random_tree(n, k):
+def random_tree(n, k, sim=True):
     N = unique_numbers()
     m = max(256, int(2**(math.ceil(math.log(n, 2))+1)))
     G = [(None, N(m))]
@@ -43,9 +43,9 @@ def random_tree(n, k):
         _, S = G[j]
     
         G.append((j, gen_set(S, i) if len(S) == m else S+N(m/2)))
-    return [(p, minhash_sig(S, k), simhash_sig(S, k), len(S) == m) for p, S in G]
+    return [(p, minhash_sig(S, k), simhash_sig(S, k) if sim else None, len(S) == m) for p, S in G]
 
-def compute_error(G, k, delta1, delta2):
+def compute_error(G, k, delta1, delta2, sim=True):
     N = len(G)
     FN1, FN2, TN = 0, 0, 0
     FP1, FP2, TP = 0, 0, 0
@@ -54,7 +54,7 @@ def compute_error(G, k, delta1, delta2):
         for j in range(i+1, N):
             nei = G[i][0] == j or G[j][0] == i
             value1 = minhash(G[i][1], G[j][1], k)
-            value2 = simhash(G[i][2], G[j][2], k)
+            value2 = simhash(G[i][2], G[j][2], k) if sim else 0
             if not nei:
                 if value1 >= delta1: FP1+=1
                 if value2 >= delta2: FP2+=1
@@ -91,7 +91,7 @@ def test_delta(N, k):
         print '\t'.join(str(x) for x in (delta/float(X), avg(RP1[delta]), avg(RN1[delta]), avg(RP2[delta]), avg(RN2[delta])))
 
 def test_size(k):
-    X = 40
+    X = 20
     RP1 = [[] for i in xrange(X+1)]
     RN1 = [[] for i in xrange(X+1)]
     RP2 = [[] for i in xrange(X+1)]
@@ -101,8 +101,8 @@ def test_size(k):
         for delta in range(1, X+1):
             sys.stderr.write(str(test)+' '+str(delta)+'\n')    
             N = int(delta/float(X)*200)
-            G = random_tree(N, k)
-            FP1, FN1, FP2, FN2 = compute_error(G, k, 0.416666667, 0.671173911)
+            G = random_tree(N, k, False)
+            FP1, FN1, FP2, FN2 = compute_error(G, k, 0.375, 0.671173911, False)
             RP1[delta].append(FP1)
             RN1[delta].append(FN1)
             RP2[delta].append(FP2)
@@ -112,5 +112,30 @@ def test_size(k):
         N = int(delta/float(X)*200)
         print '\t'.join(str(x) for x in (N, avg(RP1[delta]), avg(RN1[delta]), avg(RP2[delta]), avg(RN2[delta])))
 
+def test_k():
+    X = 8
+    RP1 = [[] for i in xrange(X+1)]
+    RN1 = [[] for i in xrange(X+1)]
+    RP2 = [[] for i in xrange(X+1)]
+    RN2 = [[] for i in xrange(X+1)]
+
+    for test in range(100):
+        for delta in range(1, X+1):
+            sys.stderr.write(str(test)+' '+str(delta)+'\n')    
+            N = 200
+            k = int(delta/float(X)*128)
+            G = random_tree(N, k, False)
+            FP1, FN1, FP2, FN2 = compute_error(G, k, 0.375, 0.671173911, False)
+            RP1[delta].append(FP1)
+            RN1[delta].append(FN1)
+            RP2[delta].append(FP2)
+            RN2[delta].append(FN2)
+            
+    for delta in range(1, X+1):
+        N = int(delta/float(X)*128)
+        print '\t'.join(str(x) for x in (N, avg(RP1[delta]), avg(RN1[delta]), avg(RP2[delta]), avg(RN2[delta])))
+
+
 #test_delta(25, 128)
 test_size(128)
+#test_k()
